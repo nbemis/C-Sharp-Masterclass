d76b2039d04805b63f08b5af48e60413d5be458a
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-11-02 19:01:40 -0500

    Initial check-in of module Class_Basics

---
+++ Class_Basics/Human.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace Class_Basics
+{
+    //Public class holding Human members
+    public class Human
+    {
+        //member var
+        private string firstName;
+
+        //member var
+        private string lastName;
+
+        //member var
+        private string eyeColor;
+
+        //member var
+        private int age;
+
+        //default constructor
+        public Human()
+        {
+            Console.WriteLine("Constructor called. Object created."); 
+        }
+
+        //parameterized constructor
+        public Human(string firstName, string lastName, string eyeColor,int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public Human(string firstName, string lastName, string eyeColor)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+        
+        }
+
+        public  Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Human(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+
+
+        public void IntroduceMyself()
+        {
+            if (age != 0 && firstName != null && lastName != null && eyeColor != null)
+                Console.WriteLine("Hi, I'm {0} {1}. I have {2} eyes. I am {3} years " +
+                "old!", firstName, lastName, eyeColor, age);
+            else if (age != 0 && firstName != null && lastName != null)
+                Console.WriteLine("Hi, I'm {0} {1}. I am {2} years " +
+                "old!", firstName, lastName, age);
+            else if (firstName != null && lastName != null && eyeColor != null)
+                Console.WriteLine("Hi, I'm {0} {1}. I have {2} eyes.", firstName, lastName, eyeColor);
+            else if(firstName != null && lastName  != null)
+                Console.WriteLine("Hi, I'm {0} {1}.", firstName, lastName);
+
+        }
+    }
+}
---
+++ Class_Basics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Class_Basics
+{
+    class MainClass
+    {
+        public static void Main(string[]  args)
+        {
+            ////First instance of Human
+            //Human Noah = new Human();
+            ////set first name of object 'Noah'
+            //Noah.firstName = "Noah";
+            //Noah.IntroduceMyself();
+
+            ////Second instance of Human
+            //Human Steven = new Human();
+
+            ////set first name of object 'Steven'
+            //Steven.firstName = "Steven";
+            ////set last name of object 'Steven'
+            //Steven.lastName = "Lopez";
+            //Steven.IntroduceMyself();
+
+
+            //Human basicHuman = new Human();
+            //basicHuman.IntroduceMyself();
+
+            Human Sam = new Human("Sam", "Grape");
+            Sam.IntroduceMyself();
+
+            Human Bilbo = new Human("Harry", "Potter", "Brown", 16);
+            Bilbo.IntroduceMyself();
+
+            Human Lars = new Human("Lars", "Smith", 37);
+            Lars.IntroduceMyself();
+
+            Human Kelly = new Human("Kelly", "Jordan", "Hazel");
+            Kelly.IntroduceMyself();
+
+
+        }
+    }
+}
